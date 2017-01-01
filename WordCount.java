6ebc1ca626de9e9c6653c52a331c14436e53f616
indu <indugumber93@gmail.com>
2017-01-01 21:42:03 +0530

    Fifth test case passed

--- WordCount.java
+++ WordCount.java
@@ -6,29 +6,30 @@ import java.util.regex.Pattern;
 
 public class WordCount {
 
-  public boolean isAlphaNumeric(String s){
-    String pattern= "^[a-zA-Z!^&@$%0-9,]*$";
+  public boolean isAlphaNumeric(String s) {
+    String pattern = "^[a-zA-Z!^&@$%0-9,]*$";
     return s.matches(pattern);
   }
-  public boolean isAlpha(String s){
-    String pattern= "^[a-zA-Z0-9,]*$";
+
+  public boolean isAlpha(String s) {
+    String pattern = "^[a-zA-Z0-9]*$";
     return s.matches(pattern);
   }
 
 
-  public String validWord(String s){
-    String wordToBePrinted ="";
-    for (int i=0;i<s.length();i++)
-    {if(i+1<=s.length() && isAlpha(s.substring(i,i+1)))
-      wordToBePrinted+=s.charAt(i);
+  public String validWord(String s) {
+    String wordToBePrinted = "";
+    for (int i = 0; i < s.length(); i++) {
+      if (i + 1 <= s.length() && isAlpha(s.substring(i, i + 1)))
+        wordToBePrinted += s.charAt(i);
 
     }
-    return  wordToBePrinted;
+    return wordToBePrinted;
 
   }
 
-  private String extractWord(int startingIndexOfWord, int lastIndexOfWord, String word){
-    String wordToBeAdded ="";
+  private String extractWord(int startingIndexOfWord, int lastIndexOfWord, String word) {
+    String wordToBeAdded = "";
     if (startingIndexOfWord > word.lastIndexOf(' ')) {
       wordToBeAdded = (word.substring(startingIndexOfWord));
       System.out.println(wordToBeAdded + " last word  ");
@@ -45,28 +46,26 @@ public class WordCount {
     Map<String, Integer> putWords = new HashMap<String, Integer>();
     int lastIndexOfWord = 0;
     int startingIndexOfWord = 0;
-    int countSpaces =0;
+    int countSpaces = 0;
     countSpaces = getCountSpaces(word, countSpaces);
-    int i=0;
-    while (i<=countSpaces) {
+    int i = 0;
+    while (i <= countSpaces) {
 
-        String wordToBeAdded = extractWord(startingIndexOfWord, lastIndexOfWord,word);
-        System.out.print( "words  " + wordToBeAdded);
-      boolean validWord =isAlphaNumeric(wordToBeAdded);
+      String wordToBeAdded = extractWord(startingIndexOfWord, lastIndexOfWord, word);
+      System.out.print("words  " + wordToBeAdded);
+      boolean validWord = isAlphaNumeric(wordToBeAdded);
 
-if(validWord) {
-  String validWordToBeAdded = validWord(wordToBeAdded);
-  System.out.println("Valid Word " + validWordToBeAdded);
+      if (validWord) {
+        String validWordToBeAdded = validWord(wordToBeAdded);
+        System.out.println("Valid Word " + validWordToBeAdded);
 
-  if (putWords.containsKey(validWordToBeAdded)) {
-    // System.out.println("value of " + wordToBeAdded + putWords.get(wordToBeAdded));
-    putWords.put(validWordToBeAdded, putWords.get(validWordToBeAdded) + 1);
-  } else {
-    //  System.out.println("value of " + wordToBeAdded + putWords.get(wordToBeAdded));
-    putWords.put(validWordToBeAdded, 1);
-  }
-}
-            startingIndexOfWord += wordToBeAdded.length()+1; //lastIndexOfWord + 1;
+        if (putWords.containsKey(validWordToBeAdded)) {
+          putWords.put(validWordToBeAdded, putWords.get(validWordToBeAdded) + 1);
+        } else {
+          putWords.put(validWordToBeAdded, 1);
+        }
+      }
+      startingIndexOfWord += wordToBeAdded.length() + 1;
 
       i++;
     }
@@ -76,8 +75,8 @@ if(validWord) {
   }
 
   private int getCountSpaces(String word, int countSpaces) {
-    for (int i=0;i<word.length();i++)
-      if(word.charAt(i)==' ')
+    for (int i = 0; i < word.length(); i++)
+      if (word.charAt(i) == ' ')
         countSpaces++;
     return countSpaces;
   }
