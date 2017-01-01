68848f65cae5ff499df64495381a8b5c1e318548
indu <indugumber93@gmail.com>
2017-01-01 19:05:16 +0530

    Second test case passed

--- WordCount.java
+++ WordCount.java
@@ -4,10 +4,20 @@ import java.util.Map;
 
 public class WordCount {
 
-  public Map<String,Integer> phrase(String word) {
-    Map<String,Integer> putWords = new HashMap<String,Integer>();
-    putWords.put(word,1);
-    return  putWords;
+  public Map<String, Integer> phrase(String word) {
+    Map<String, Integer> putWords = new HashMap<String, Integer>();
+    int lastIndexOfWord = 0;
+    int startingIndexOfWord = 0;
+    for (int traverse =0;traverse<=word.lastIndexOf(' ') ;) {
+      lastIndexOfWord = word.indexOf(' ', startingIndexOfWord);
+      String wordToBeAdded = (word.substring(startingIndexOfWord, lastIndexOfWord));
+      putWords.put(wordToBeAdded, 1);
+      startingIndexOfWord = lastIndexOfWord + 1;
+      traverse = startingIndexOfWord;
+    }
+    String lastWord =word.substring(word.lastIndexOf(' ')+1);
+    putWords.put(lastWord,1);
+    return putWords;
 
   }
 }
